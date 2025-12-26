b97ec5c6c6f4f06fb51b6314bdab9dac5ab3b95a
Aniruth <aniruths10@gmail.com>
2025-12-26 14:12:19 -0800

    Time: 0 ms (100%), Space: 17.8 MB (95.77%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -3,16 +3,12 @@ class Solution:
         trips.sort(key=lambda x: x[1])
         heap = []
         current_passengers = 0
-
         for num_passengers, start, end in trips:
             while heap and heap[0][0] <= start:
                 drop_end, drop_num = heapq.heappop(heap)
                 current_passengers -= drop_num
-
             heapq.heappush(heap, [end, num_passengers])
             current_passengers += num_passengers
-
             if current_passengers > capacity:
                 return False
-
         return True
