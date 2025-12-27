72a47b28f4077496f0e9a313f99c28660ce4ceb1
Aniruth <aniruths10@gmail.com>
2025-12-27 00:57:59 -0800

    Time: 0 ms (100%), Space: 17.1 MB (98.97%) - LeetHub

--- 2591-distribute-money-to-maximum-children/2591-distribute-money-to-maximum-children.py
+++ 2591-distribute-money-to-maximum-children/2591-distribute-money-to-maximum-children.py
@@ -1,27 +1,14 @@
 class Solution:
     def distMoney(self, money: int, children: int) -> int:
-        if (children > money):
-            return -1
-        child = [1 for i in range(children)]
-        money-=children 
-        i = 0
-        while money>0:
-            if (child[i] < 8) and money>=8:
-                money-=(8 - child[i]) 
-                child[i] = 8 
 
-            if i==children-1:
-                if money > 0:
-                    if child[i]+money!=4:
-                        child[i]+=money 
-                        money = 0
-                    else:
-                        child[i-1]+=money 
-                        money=0 
-            i+=1
-        ct = 0 
-        print(child)
-        for i in child:
-            if i==8:
-                ct+=1 
-        return ct 
+        if children > money:
+            return -1 
+        
+        n = (8*children) - money 
+
+        if n<=0:
+            return children - (n<0)
+        
+        num , rem = divmod(money - children , 7)
+
+        return num - ((num, rem) == (children - 1, 3))
