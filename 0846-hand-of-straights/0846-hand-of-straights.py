0b0863de0f5b83dabe12a681776a0db77a53a7ad
Aniruth <aniruths10@gmail.com>
2025-12-27 18:33:05 -0800

    Time: 27 ms (92.84%), Space: 19.4 MB (19.55%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -4,25 +4,12 @@ class Solution:
         if len(hand) % groupSize != 0:
             return False
         counter = Counter(hand)
-        num_groups = len(hand)//groupSize 
-        group_num = 0 
-        hand.sort()
-        for i in range(len(hand)):
-            char = hand[i]
-            if counter[char]>0:
-                num = char
-                flag = None
-                for j in range(groupSize):
-                    if not(num+j in counter and counter[num+j] > 0):
-                        flag = True
+        for card in sorted(counter):
+            f = counter[card]
+            if counter[card] > 0:
+                for next_card in range(card , card + groupSize):
+                    if counter[next_card] < f:
                         return False 
-                if flag is None:
-                    group_num+=1
-                    for i in range(groupSize):
-                        counter[char]-=1
-                        char+=1 
-        
-        print(group_num , num_groups)
-        if group_num == num_groups:
-            return True 
-        return False
+                    counter[next_card]-=f
+        return True
+            
