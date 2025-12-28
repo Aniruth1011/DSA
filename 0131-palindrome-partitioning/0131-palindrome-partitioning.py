b953b99595299d13fe22d7e350f69013a0a07779
Aniruth <aniruths10@gmail.com>
2025-12-28 14:49:27 -0800

    Time: 61 ms (18.39%), Space: 32.8 MB (88.73%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,22 +1,15 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-
-        def ispalindrome(s):
-            if s == s[::-1]:
-                return True 
-            return False 
-        
         result = []
         def backtrack(start , path):
             if start == len(s):
                 result.append(path[:])
-                return 
-            
-            for end in range(start+1 , len(s)+1):
-                if ispalindrome(s[start:end]):
+            for end in range(start + 1 , len(s) + 1):
+                if s[start:end] ==  s[start:end][::-1]:
                     path.append(s[start:end])
                     backtrack(end , path)
                     path.pop()
-        
+            return result 
+    
         backtrack(0 , [])
         return result 
