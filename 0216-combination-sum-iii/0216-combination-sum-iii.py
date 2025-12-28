4e0ba106fb9edb4dd76495883d0a2672f609dee9
Aniruth <aniruths10@gmail.com>
2025-12-28 12:28:22 -0800

    Time: 2 ms (27.58%), Space: 17.4 MB (88.39%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -7,10 +7,12 @@ class Solution:
                 result.append(path[:]) 
                 return 
             
-            for i in range(start , 10):
-                path.append(i)
-                backtrack(i+1 , path)
-                path.pop()
+            if not ((len(path)>k) or (sum(path)>n)):
+                
+                for i in range(start , 10):
+                    path.append(i)
+                    backtrack(i+1 , path)
+                    path.pop()
             
         backtrack(1 , [])
         return result 
