056044cfc2ccd88473696672d8cda7a68babc9ee
Aniruth <aniruths10@gmail.com>
2025-12-28 14:56:04 -0800

    Time: 59 ms (23.86%), Space: 33.1 MB (83.27%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -4,12 +4,12 @@ class Solution:
         def backtrack(start , path):
             if start == len(s):
                 result.append(path[:])
+                return 
             for end in range(start + 1 , len(s) + 1):
                 if s[start:end] ==  s[start:end][::-1]:
                     path.append(s[start:end])
                     backtrack(end , path)
                     path.pop()
-            return result 
-    
+
         backtrack(0 , [])
         return result 
