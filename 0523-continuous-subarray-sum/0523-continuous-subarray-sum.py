989e41bba85796118d5e805536c86492a3dae5da
Aniruth <aniruths10@gmail.com>
2026-01-02 12:16:02 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        dic = {}
+        s=0
+        for i , num in enumerate(nums):
+            s+=num 
+            rem = s% k 
+            if rem in dic:
+                if i-dic[rem]>=2:
+                    return True 
+            else:
+                dic[rem] = i 
+        return False
