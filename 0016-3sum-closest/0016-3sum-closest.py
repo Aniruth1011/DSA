ba1ceae98c15eb36ea9826c7f270ad0030a1b0fd
Aniruth <aniruths10@gmail.com>
2026-01-10 14:47:34 -0800

    Time: 379 ms (26.19%), Space: 19.4 MB (6.73%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -4,19 +4,20 @@ class Solution:
         n = len(nums)
         best_sum = nums[0] + nums[1] + nums[2]
         most_closest = abs(best_sum - target)
-        for idx , num1 in enumerate(nums):
+        for idx in range(n-2):
             num1 = nums[idx]
             left = idx+1
             right = n-1
             while (left < right):
                 num2 = nums[left]
                 num3 = nums[right]
-                if abs(num1 + num2 + num3 - target) < most_closest:
-                    most_closest = abs(num1 + num2 + num3 - target)
-                    best_sum = num1 + num2 + num3
+                total_sum = num1 + num2 + num3
+                if abs( total_sum - target) < most_closest:
+                    most_closest = abs(total_sum - target)
+                    best_sum = total_sum
                 if (num1+num2+num3) == target:
-                    return num1 + num2 + num3
-                elif (num1+num2+num3) < target:
+                    return total_sum
+                elif (total_sum) < target:
                     left+=1 
                 else:
                     right-=1
