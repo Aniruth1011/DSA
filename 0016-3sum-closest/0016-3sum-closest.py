43fcf3f2ba83574956d8e031e6ba3d48c2e8f49d
Aniruth <aniruths10@gmail.com>
2026-01-10 14:45:56 -0800

    Time: 440 ms (10.7%), Space: 19.4 MB (6.73%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -1,34 +1,23 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-
-
+        nums.sort()
         n = len(nums)
-        if n==3:
-            return sum(nums) 
-        nums = sorted(nums)
-        most_closest = inf
-        triplet_sum = 0
-        for i in range(n-2):
-            el = nums[i]
-            l = i+1
-            r = n-1
-            while (l<r):
-                el1 = nums[l]
-                el2 = nums[r]
-                s = el1 + el2 + el
-                diff = abs(s - target) 
-                sum_ = el1 + el2 + el
-                if most_closest > diff: 
-                    most_closest = diff 
-                    triplet_sum = el + el1 + el2 
-                
-                if s>target:
-                    r-=1
-                elif s<target:
-                    l+=1
+        best_sum = nums[0] + nums[1] + nums[2]
+        most_closest = abs(best_sum - target)
+        for idx , num1 in enumerate(nums):
+            num1 = nums[idx]
+            left = idx+1
+            right = n-1
+            while (left < right):
+                num2 = nums[left]
+                num3 = nums[right]
+                if abs(num1 + num2 + num3 - target) < most_closest:
+                    most_closest = abs(num1 + num2 + num3 - target)
+                    best_sum = num1 + num2 + num3
+                if (num1+num2+num3) == target:
+                    return num1 + num2 + num3
+                elif (num1+num2+num3) < target:
+                    left+=1 
                 else:
-                    return target 
-        
-        return triplet_sum
-            
-                
+                    right-=1
+        return best_sum 
