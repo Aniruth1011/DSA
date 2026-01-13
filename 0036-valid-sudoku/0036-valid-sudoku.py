c4219b72069e26410d3989f2246aec743dcf777f
Aniruth <aniruths10@gmail.com>
2026-01-13 13:31:18 -0800

    Time: 3 ms (81.33%), Space: 19.6 MB (5.87%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,30 +1,23 @@
 class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-
-        row = [set() for i in range(9)]
-        column = [set() for i in range(9)]
-        boxes = [set() for i in range(9)]
+    def isValidSudoku(self, board: list[list[str]]) -> bool:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
 
         for i in range(9):
             for j in range(9):
-                if board[i][j]!=".":
-                    if board[i][j] in row[i]:
-                        return False 
-                    row[i].add(board[i][j])
-        
-        for i in range(9):
-            for j in range(9):
-                if board[j][i]!=".":
-                    if board[j][i] in column[i]:
-                        return False 
-                    column[i].add(board[j][i])
-        
-        for i in range(9):
-            for j in range(9):
-                box_number = (i//3) + (j//3)*3 
-                if board[i][j]!=".":
-                    if board[i][j] in boxes[box_number]:
-                        return False 
-                    boxes[box_number].add(board[i][j])
-        return True 
+                val = board[i][j]
+                if val == ".":
+                    continue
+                if val in rows[i]:
+                    return False
+                rows[i].add(val)
+                if val in cols[j]:
+                    return False
+                cols[j].add(val)
+                box_index = (i // 3) * 3 + (j // 3)
+                if val in boxes[box_index]:
+                    return False
+                boxes[box_index].add(val)
 
+        return True
