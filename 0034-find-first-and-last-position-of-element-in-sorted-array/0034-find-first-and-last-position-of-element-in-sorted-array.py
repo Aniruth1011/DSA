a39453ad9df24da9aa699bc8b5b023a122b347e9
Aniruth <aniruths10@gmail.com>
2026-01-13 13:04:59 -0800

    Time: 0 ms (100%), Space: 20.8 MB (6.58%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,50 +1,28 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-
         n = len(nums)
-
-        if n==0:
+        left = 0 
+        right = n-1 
+        idx = -1
+        while left <= right:
+            mid = (left + right)//2 
+            if nums[mid] == target:
+                idx = mid  
+                break
+            elif nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1 
+        
+        if idx == -1:
             return [-1 , -1]
-        elif n==1:
-            if nums[0] == target:
-                return [0 , 0]
-            else:
-                return [-1,-1]
-        elif n==2:
-            if nums[0] == nums[1] :
-                if nums[0] == target:
-                    return [0,1]
-            else:
-                if nums[0] == target:
-                    return [0,0]
-                elif nums[1] == target:
-                    return [1,1]
-                return [-1,-1]
-        l = 0 
-        r = n-1 
+        base_idx1 = idx  
+        while (base_idx1>=0) and (nums[base_idx1] == target) : 
+            base_idx1-=1
 
-        while (l<=r):
-            med = (l+r)//2 
-            if (nums[med] == target):
-                value1 = med
-                value2 = med
-                res = []
-                if med >0:
-                    while (nums[value1]==nums[value1-1]):
-                        value1-=1
-                        if value1 == 0:
-                            break
-                starting = value1 
-                if med < n-1:
-                    while (nums[value2] == nums[value2+1]):
-                        value2+=1 
-                        if value2 == n-1:
-                            break 
-                ending = value2 
-                return [starting , ending] 
+        base_idx2 = idx 
+        while (base_idx2<n) and (nums[base_idx2] == target) : 
+            base_idx2+=1
+        
+        return [base_idx1+1 , base_idx2-1]
 
-            elif (nums[med] < target):
-                l = med+1
-            else:
-                r = med-1
-        return [-1,-1]
