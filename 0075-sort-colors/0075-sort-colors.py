4b2546645f7266cd91e6d5484a2963fa968ab2ae
Aniruth <aniruths10@gmail.com>
2026-01-15 14:33:16 -0800

    Time: 0 ms (100%), Space: 19.6 MB (10.84%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,23 +1,15 @@
+from collections import Counter
 class Solution:
-    def partition(self , nums ,  left , right):
-        pivot_element = nums[right]
-        i = left - 1
-        for j in range(left , right):
-            if nums[j] <= pivot_element:
-                i+=1 
-                nums[i] , nums[j] = nums[j] , nums[i]
-        nums[i+1] , nums[right] = nums[right] , nums[i+1]
-        return i+1 
-
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        left = 0 
-        high = len(nums)-1 
-        def quicksort(nums , low , high):
-            if low < high:
-                pi = self.partition(nums, low, high)
-                quicksort(nums, low, pi - 1)    
-                quicksort(nums, pi + 1, high)  
-        quicksort(nums , 0 , high)
+        counter = Counter(nums)
+        num_0 = counter[0]
+        num_1 = counter[1]
+        num_2 = counter[2]
+        print(num_0 , num_1 , num_2)
+        nums[:num_0] = [0] * num_0 
+        nums[num_0:num_0+num_1] = [1] * num_1 
+        nums[num_0+num_1:num_0+num_1 + num_2] = [2] * num_2
+
