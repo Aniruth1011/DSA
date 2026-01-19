3f935908f5d5a1943eb7868a99f5b9a79ee7249e
Aniruth <aniruths10@gmail.com>
2026-01-19 15:25:31 -0800

    Time: 249 ms (5.03%), Space: 30.6 MB (10.18%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,18 +1,27 @@
 class Solution:
-    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+    def binary_search(self , target , num):
         l = 0 
-        r = 0 
+        r = len(target) - 1
+        while (l<r):
+            mid = (l+r)//2 
+            if target[mid] < num:
+                l = mid + 1
+            else:
+                r = mid
+        return l 
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         n = len(nums)
-        s = 0
+        prefix_sum = [0] * (n+1)
+        for i in range(1 , n+1):
+            prefix_sum[i] = prefix_sum[i-1] + nums[i-1]
         min_len = float('inf')
-        while r < n:
-            s+= nums[r]
-            while s >=target:
-                min_len = min(min_len , r - l + 1)
-                s-=nums[l]
-                l+=1 
-            r+=1 
+        for i in range(n):
+            required = prefix_sum[i] + target
+            j = self.binary_search(prefix_sum , required)
+            if j <= n and prefix_sum[j] >= required:
+                min_len = min(min_len, j - i)
         
         if min_len == float('inf'):
             return 0 
         return min_len 
+
