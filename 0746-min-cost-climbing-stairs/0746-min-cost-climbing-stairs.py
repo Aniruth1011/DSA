c196bc33a9a60cc1f9613454c9158c6dcc45df71
Aniruth <aniruths10@gmail.com>
2026-03-07 14:50:26 -0800

    Time: 7 ms (17.7%), Space: 21.8 MB (14.96%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,11 +1,34 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        memo = {}
+        def dp(n):
+            if n in memo:
+                return memo[n]
+            if n==0:
+                return cost[0]
+            elif n==1:
+                return cost[1]
+            else:
+                memo[n] =  cost[n] + min(dp(n-1), dp(n-2)) 
+                return memo[n]
         n = len(cost)
-
-        dp = [0 for i in range(n+1)]
-
-        for i in range(2 , n+1):
-            dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2])
+        return min(dp(n-1) , dp(n-2))
         
-        return dp[n]
-    
+        # n = len(cost)
+        # if n<=3:
+        #     return cost[0] 
+        # prev_2 = cost[2]
+        # prev_1 = cost[1]
+        # min_cost1 = 0
+        # for i in range(3,n):
+        #     min_cost1 = min_cost1 + min(prev_1 , prev_2)
+        #     prev_1 , prev_2 = cost[i] , prev_1 
+        # prev_2 = cost[1]
+        # prev_1 = cost[0]
+        # min_cost = 0
+        # for i in range(2,n):
+        #     min_cost = min_cost + min(prev_1 , prev_2)
+        #     prev_1 , prev_2 = cost[i] , prev_1
+        # print(min_cost,min_cost1)
+        # return min(min_cost,min_cost1)
+        
