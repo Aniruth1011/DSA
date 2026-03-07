e8c689bcf0fe67931d0811117bb1ad0b85e2c106
Aniruth <aniruths10@gmail.com>
2026-03-07 14:09:46 -0800

    Time: 0 ms (100%), Space: 19.3 MB (51.66%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -2,10 +2,10 @@ class Solution:
     def climbStairs(self, n: int) -> int: 
         if n<=2:
             return n
-        opt = [0 for i in range(n)]
-        opt[0] = 1
-        opt[1] = 2
+        prev_2 = 1
+        prev_1 = 2
         for i in range(2,n):
-            opt[i] = opt[i-1] + opt[i-2]
-        return opt[n-1]
+            next_val = prev_1 + prev_2 
+            prev_1 , prev_2 = next_val, prev_1
+        return next_val
         
