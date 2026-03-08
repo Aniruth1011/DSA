bcdb9c31a630d64c7be9d44c1a6d5fbb32950734
Aniruth <aniruths10@gmail.com>
2026-03-07 18:44:50 -0800

    Time: 2 ms (24.56%), Space: 19.7 MB (14.89%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,10 +1,38 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        grid  = [[1 for i in range(n)] for j in range(m)]
+        if m==1 and n==1:
+            return 1
+        dp = [[0 for i in range(n)] for j in range(m)]
+        def dp_fun(x,y):
+            if x==0 and y==0:
+                return 0 
+            elif x==0 and y==1:
+                return 1
+            elif x==1 and y==0:
+                return 1 
+            else:
+                dp[x][y] = dp_fun(x-1,y) + dp_fun(x,y+1) + 2 
+                return dp[x][y]
+        
+        #return dp_fun(m-1 , n-1)
+        for x in range(m):
+            for y in range(n):
+                if x==0 and y==0:
+                    dp[x][y] = 0 
+                elif x==0 and y==1:
+                    dp[x][y] = 1
+                elif x==1 and y==0:
+                    dp[x][y] = 1
+                else:
+                    if (x==0) :
+                        dp[x][y] = dp[x][y-1]
+                    elif (y==0) :
+                        dp[x][y] = dp[x-1][y]
+                    else:
+                        dp[x][y] = dp[x][y-1] + dp[x-1][y] 
+                # print(x,y,dp[x][y])    
 
-        for i in range(1 , m):
-            for j in range(1 , n):
-                grid[i][j] = grid[i][j-1] + grid[i-1][j]
+        for i in range(m):
+            print(dp[i])
 
-        return grid[m-1][n-1]
-
+        return dp[m-1][n-1]
