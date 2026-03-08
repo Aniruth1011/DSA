77b94b8c8e80ccf2bc57a4337f50dde8290869a1
Aniruth <aniruths10@gmail.com>
2026-03-08 14:16:30 -0700

    Time: 13 ms (76.91%), Space: 20.1 MB (89.78%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -1,15 +1,29 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         m , n = len(matrix) , len(matrix[0])
-        dp = [[0 for i in range(n)] for j in range(m)]
+        dp = [0 for i in range(n)]
         for x in range(m):
+            prev_row = dp[:]
             for y in range(n):
                 if x==0:
-                    dp[x][y] = matrix[x][y]
-                if y==0:
-                    dp[x][y] = matrix[x][y] + min(dp[x-1][y] , dp[x-1][y+1])
+                    dp[y] = matrix[x][y]
+                elif y==0:
+                    dp[y] = matrix[x][y] + min(prev_row[y] , prev_row[y+1])
                 elif y==n-1:
-                    dp[x][y] = matrix[x][y] + min(dp[x-1][y] , dp[x-1][y-1])
+                    dp[y] = matrix[x][y] + min(prev_row[y] , prev_row[y-1])
                 else:
-                    dp[x][y] = matrix[x][y] + min(dp[x-1][y-1] , dp[x-1][y] , dp[x-1][y+1]) 
-        return min(dp[-1])
+                    dp[y] = matrix[x][y] + min(prev_row[y] , prev_row[y-1] , prev_row[y+1])
+            print(dp)
+        return min(dp)
+        # dp = [[0 for i in range(n)] for j in range(m)]
+        # for x in range(m):
+        #     for y in range(n):
+        #         if x==0:
+        #             dp[x][y] = matrix[x][y]
+        #         elif y==0:
+        #             dp[x][y] = matrix[x][y] + min(dp[x-1][y] , dp[x-1][y+1])
+        #         elif y==n-1:
+        #             dp[x][y] = matrix[x][y] + min(dp[x-1][y] , dp[x-1][y-1])
+        #         else:
+        #             dp[x][y] = matrix[x][y] + min(dp[x-1][y-1] , dp[x-1][y] , dp[x-1][y+1]) 
+        # return min(dp[-1])
