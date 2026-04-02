9784e3d55b90f05324ef823dff272910a3e81ca9
Aniruth <aniruths10@gmail.com>
2026-04-02 15:09:40 -0700

    Time: 16 ms (56.85%), Space: 20.1 MB (84.65%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -1,19 +1,39 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        m , n = len(matrix) , len(matrix[0])
-        dp = [0 for i in range(n)]
-        for x in range(m):
+        m , n=  len(matrix) , len(matrix[0])
+        dp = matrix[0][:]
+        for i in range(1 , m):
             prev_row = dp[:]
-            for y in range(n):
-                if x==0:
-                    dp[y] = matrix[x][y]
-                elif y==0:
-                    dp[y] = matrix[x][y] + min(prev_row[y] , prev_row[y+1])
-                elif y==n-1:
-                    dp[y] = matrix[x][y] + min(prev_row[y] , prev_row[y-1])
+            for j in range(n):
+                if j==0:
+                    dp[j] = matrix[i][j] +  min(prev_row[j] , prev_row[j+1])
+                elif j == n-1:
+                    dp[j] = matrix[i][j] + min(prev_row[j] , prev_row[j-1])
                 else:
-                    dp[y] = matrix[x][y] + min(prev_row[y] , prev_row[y-1] , prev_row[y+1])
+                    dp[j] = matrix[i][j] + min(prev_row[j] , prev_row[j+1] , prev_row[j-1]) 
             print(dp)
+        return min(dp)
+
+        # 2 1 3 
+        # 6 5 4 
+        # 7 8 9  
+
+        #
+
+
+        # m , n = len(matrix) , len(matrix[0])
+        # dp = [0 for i in range(n)]
+        # for x in range(m):
+        #     prev_row = dp[:]
+        #     for y in range(n):
+        #         if x==0:
+        #             dp[y] = matrix[x][y]
+        #         elif y==0:
+        #             dp[y] = matrix[x][y] + min(prev_row[y] , prev_row[y+1])
+        #         elif y==n-1:
+        #             dp[y] = matrix[x][y] + min(prev_row[y] , prev_row[y-1])
+        #         else:
+        #             dp[y] = matrix[x][y] + min(prev_row[y] , prev_row[y-1] , prev_row[y+1])
         return min(dp)
         # dp = [[0 for i in range(n)] for j in range(m)]
         # for x in range(m):
